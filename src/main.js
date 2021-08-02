ddef4071c6f995217d27645e64545ce1d26b0eb4
bp_yeonho <yeonho@in-soft.co.kr>
2021-08-03 04:19:49 +0900

    1. 알브하임 사냥터 업데이트 2. 초기화 버튼 추가 3. sass 모듈 추가 4. 스타일을 scss로 변경 5. UI 스타일 일부 수정

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import router from '@/router/router'
 import element from '@/plugins/element-plus'
 import App from './App.vue'
 
+import '@/styles/index.scss'
+
 createApp(App)
 .use(router)
 .use(element)
