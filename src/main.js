c1928b7b58fd8ee754227d86211f8b7f7624253e
bp_yeonho <yeonho@in-soft.co.kr>
2021-08-09 17:15:59 +0900

    1.1 패치

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import router from '@/router/router'
 import element from '@/plugins/element-plus'
-import Vue3Autocounter from 'vue3-autocounter';
 import App from './App.vue'
 
 import '@/styles/index.scss'
@@ -9,5 +8,4 @@ import '@/styles/index.scss'
 createApp(App)
 .use(router)
 .use(element)
-.component('vue3-autocounter', Vue3Autocounter)
 .mount('#app')
